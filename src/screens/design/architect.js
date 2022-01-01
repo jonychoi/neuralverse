fc7b91dcee6e5c135931226700c4afa7be14ebc0
John Steve <buddhychoi@gmail.com>
2022-01-01 20:52:44 +0900

    update

--- src/screens/design/architect.js
+++ src/screens/design/architect.js
@@ -1,2 +1,97 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import {Col, Row, Flex, Text, Image} from '../../components/common/base';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+// fake data generator(가짜 데이터 제너레이터)
+const getItems = (count) => Array.from({length: count}, (v, k) => k).map(k => ({
+  id: `item-${k}`,
+  content: `item ${k}`
+}));
+
+// a little function to help us with reordering the result(결과 재정렬을 돕는 함수)
+const reorder =  (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+// using some little inline style helpers to make the app look okay(보기좋게 앱을 만드는 인라인 스타일 헬퍼)
+const grid = 8;
+const getItemStyle = (draggableStyle, isDragging) => ({
+  // some basic styles to make the items look a bit nicer(아이템을 보기 좋게 만드는 몇 가지 기본 스타일)
+  userSelect: 'none',
+  padding: grid * 2,
+  marginBottom: grid,
+
+  // change background colour if dragging(드래깅시 배경색 변경)
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables(드래그에 필요한 스타일 적용)
+  ...draggableStyle
+});
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 250
+});
+
+export const Architect = () => {
+    const [state, setState] = useState({items: getItems(10)})
+
+    const onDragEnd = (result) => {
+        // dropped outside the list(리스트 밖으로 드랍한 경우)
+        if(!result.destination) {
+        return;
+    }
+
+    const items = reorder(
+      state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    setState(items)
+  }
+
+return (
+    <DragDropContext onDragEnd={() => onDragEnd()}>
+        <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+            <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+            >
+                {state.items.map(item => (
+                <Draggable
+                    key={item.id}
+                    draggableId={item.id}
+                >
+                    {(provided, snapshot) => (
+                    <div>
+                        <div
+                        ref={provided.innerRef}
+                        style={getItemStyle(
+                            provided.draggableStyle,
+                            snapshot.isDragging
+                        )}
+                        {...provided.dragHandleProps}
+                        >
+                        {item.content}
+                        </div>
+                        {provided.placeholder}
+                    </div>
+                    )}
+                </Draggable>
+                ))}
+            </div>
+            )}
+        </Droppable>
+        </DragDropContext>
+    );
+};
+
+
+export default Architect;
+
--- src/screens/design/components.js
+++ src/screens/design/components.js
@@ -5,7 +5,7 @@ import Button from '../../components/buttons/button';
 import {bgStyler, hueStyler, btnStyler, blueColor, redColor, purLowColor, purColor, blueLowColor} from '../../styles';
 import {RightIcon, BenchmarkIcon, SearchIcon, CloseIcon} from '../../icons/utilities';
 import styled from 'styled-components';
-import { Height } from '../../hooks/getWindow';
+import Architect from './architect';
 
 const Box = styled(Col)`
     transition: background-color .3s,color .3s,box-shadow .3s;
@@ -34,7 +34,7 @@ export const Models = ({modelVisual, setModelVisual, darkMode, title}) => {
     return (
         <Col>
             <Text size="22" className="bold" weight="600">TRENDING MODELS</Text>
-
+            <Architect />
         </Col>
     )
 }
@@ -60,7 +60,7 @@ export const TaskMenu = ({darkMode}) => {
             <Row align="center">
                 <Text size="22" className="bold" weight="600" margin="margin-right: 15px;">COMPUTER VISION</Text>
                 <Flex width="10px" height="15px" position="relative">
-                    <Flex position="absolute" style={{transition: 'all 500ms', opacity: !darkMode ? 1 : 0}}>
+                    <Flex position="absolute" style={{transition: 'all 500ms', opacity: !darkMode ? 1 : 0}}>    
                         <RightIcon width={10} height={15} stroke="black" />
                     </Flex>
                     <Flex position="absolute" style={{transition: 'all 500ms', opacity: darkMode ? 1 : 0}}>
@@ -83,7 +83,6 @@ export const TaskMenu = ({darkMode}) => {
 export const Design = ({setSideBar, sideBar, darkMode}) => {
     return (
         <Col>
-            <SideBar setSideBar={setSideBar} sideBar={sideBar} darkMode={darkMode} />
             <Row align="flex-end">
                 <Col flex={3}>
                     <Text size="30" className="bold">DESIGN YOUR SOTA</Text>
@@ -96,73 +95,6 @@ export const Design = ({setSideBar, sideBar, darkMode}) => {
                 <Models darkMode={darkMode} />
             </Col>
         </Col>
-    )
-}
-
-
-export const Model = ({modelVisual, setModelVisual, darkMode}) => {
-    return (
-        <Box margin="margin: 10px 0px;" padding="padding: 15px" style={{flexDirection: 'row', height: 180}} bg={bgStyler(darkMode)} br="20px;">
-            <Col>
-                <Row align="center">
-                    <Flex flex={1} style={{maxHeight: 100, maxWidth: 100, overflow: 'hidden'}} br="10px" align="center" justify="center">
-                        <Image src={gan} style={{height: '100%', width: '100%'}} of="cover" />
-                    </Flex>
-                    <Flex flex={6} margin="margin-left: 10px;">
-                        <Text className="bold" size="14">
-                            JoJoGAN: One Shot Face Stylization
-                        </Text>
-                    </Flex>
-                </Row>
-                <Text style={{opacity: 0.85}} size="12.8" className="light" weight="500" margin="margin: 10px 0px;">
-                    mchong6/JoJoGAN •  • arXiv 2021
-                </Text>
-                <Text margin="margin-bottom: 5px;" size="12.8" style={{height: 50, overflow: 'hidden'}}>
-                    While there have been recent advances in few-shot image stylization, these methods
-                    fail to capture stylistic details that are obvious to humans. Details such as the shape
-                    of the eyes, the boldness of the lines, are especially difficult for a model to learn,
-                    especially so under a limited data setting. In this work, we aim to perform oneshot image stylization that gets the details right. Given a reference style image,
-                    we approximate paired real data using GAN inversion and finetune a pretrained
-                    StyleGAN using that approximate paired data. We then encourage the StyleGAN
-                    to generalize so that the learned style can be applied to all other images.                    
-                </Text>
-                <Row margin="margin: 5px 0px;">
-                    <Text>GAN Inversion</Text>
-                    <Text>Image Stylization</Text>
-                </Row>
-            </Col>
-        </Box>
-    )
-}
-
-const sideBarHeight = Height - 80;
-
-export const SideBar = ({sideBar, setSideBar, darkMode}) => {
-    const [model, setModel] = useState(true);
-    return (
-        <Box position="fixed" padding="padding: 20px;" style={{zIndex: 100, right: sideBar ? 0 : - 400, width: 400, height: sideBarHeight, transition: 'all 300ms ease', top: 0, marginTop: 80, overflowY: 'scroll'}} bg={darkMode ? "black" : "white"}>
-            <Flex position="absolute" onClick={() => setSideBar(false)} style={{right: 20, top: 20}} to="cursor">
-                <CloseIcon bg={bgStyler(darkMode)} stroke={"white"} width={23} height={23} />
-            </Flex>
-            <Col>
-                <Row align="center" justify="space-between" margin="margin-right: 40px;">
-                    <Text to="cursor" size="20" className="bold" margin="margin-bottom: 10px;">{model == true ? "My Models" : "My Datasets"}</Text>
-                    <Text to="cursor" onClick={() => setModel(!model)} size="20" className="bold" size="12.8" color="rgb(100, 100, 100)" margin="margin-bottom: 10px;">{model == true ? "My Datasets" : "My Models"}</Text>
-                </Row>
-                <Model darkMode={darkMode} />
-                <Model darkMode={darkMode} />
-                <Model darkMode={darkMode} />
-                <Flex justify="center"><Text>More</Text></Flex>
-            </Col>
-            <Col margin="margin-top: 20px;">
-                <Text size="20" className="bold" margin="margin-bottom: 10px;">Saved</Text>
-                <Model darkMode={darkMode} />
-                <Model darkMode={darkMode} />
-                <Model darkMode={darkMode} />
-                <Model darkMode={darkMode} />
-                <Model darkMode={darkMode} />
-            </Col>
-        </Box>
     )
 }
 
