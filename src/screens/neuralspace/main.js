805c4fd8a6f3ff93eb2b52ceb93d7ee19bb5e936
Jonathan Choi <51044058+koreaunitas@users.noreply.github.com>
2022-01-26 00:00:19 +0900

    update

--- src/screens/neuralspace/main.js
+++ src/screens/neuralspace/main.js
@@ -4,6 +4,7 @@ import {SearchIcon} from '../../icons/utilities';
 import {bgStyler} from '../../styles';
 import tasks from '../../data/tasks';
 import styled from 'styled-components';
+import segmentationimg from '../../data/tasks/imgs/segmentation.jpg'
 
 export const TaskBox = styled(Col)`
     opacity: ${(props) => props.task == props.title ? 1 : 1};
@@ -45,7 +46,8 @@ export const Selector = ({darkMode, task, setTask, subTask, setSubTask}) => {
 export const TaskIntroduction = ({}) => {
     return (
         <Col flex={3}>
-        
+            <Image width="500px" src={segmentationimg} />
+            <Text>Semantic segmentation, or image segmentation, is the task of clustering parts of an image together which belong to the same object class. It is a form of pixel-level prediction because each pixel in an image is classified according to a category. Some example benchmarks for this task are Cityscapes, PASCAL VOC and ADE20K. Models are usually evaluated with the Mean Intersection-Over-Union (Mean IoU) and Pixel Accuracy metrics.</Text>
         </Col>
     )
 }
@@ -70,15 +72,28 @@ export const Task = ({darkMode, content, task}) => {
 
 export const Categories = ({darkMode, task, setTask, subTask, setSubTask}) => {
     return (
-        <Box flex={1} position="relative" height="400px" br="20px" style={{overflow: 'hidden'}}>
+        <Box flex={1} position="relative" height="480px" br="20px" style={{overflow: 'hidden'}}>
+            <Flex flex={1} position="relative" height="35px" margin="margin-top: 20px;">
+                <Flex position="absolute" style={{left: 0, bottom: 10}}>
+                    <SearchIcon width={18} height={18} stroke="rgb(100, 100, 100)" />
+                </Flex>
+                <Input
+                    padding="padding: 10px 0px; padding-left: 30px;" 
+                    color={darkMode ? "white" : "black"} bg="rgba(0,0,0,0)" 
+                    style={{border: 0, borderBottomWidth: 1.5, borderBottomColor: 'rgb(100, 100, 100)', borderBottomStyle: 'solid'}} 
+                    height="35px;" placeholder="Search" width="100%"
+                />
+            </Flex>
             <Col width="100%" height="480px" bg={bgStyler(darkMode)} 
                 position="absolute" scrollbarFalse={true} 
+                margin="margin-top: 80px;"
                 style={{overflow: 'hidden', overflowY: 'scroll', display: 'block'}}>
                 {tasks.map((item) => 
                 <div onClick={() => setTask(item.title)}><Task task={task} key={item.title} content={item} darkMode={darkMode} /></div>)}
             </Col>
             <Col width="100%" height="480px" bg={bgStyler(darkMode)} 
                 position="absolute" scrollbarFalse={true} 
+                margin="margin-top: 80px;"
                 style={{overflow: 'hidden', overflowY: 'scroll', display: 'block', right: '-100%', transition: 'all 300ms'}}>
                 {tasks.map((item) => 
                 <div onClick={() => setTask(item.title)}><Task task={task} key={item.title} content={item} darkMode={darkMode} /></div>)}
@@ -105,7 +120,10 @@ export const Model = ({darkMode}) => {
 export const Models = ({darkMode, task, subTask}) => {
     return (
         <Col flex={3} margin="margin-top: 50px;">
-            <Text>Trendings on {subTask ? subTask : task ? task : "All Computer Vision"}</Text>
+            <Row>
+                <Text>Icon</Text>
+                <Text className="bold" size="25" margin="margin: 30px 0px;">Trendings on {subTask ? subTask : task ? task : "All Computer Vision"}</Text>
+            </Row>
             <Row>
                 <Model darkMode={darkMode} />
                 <Model darkMode={darkMode} />
