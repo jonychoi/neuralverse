dc02904faf779ce266fe9d3d5477e928a7c46c85
Jonathan Choi <51044058+koreaunitas@users.noreply.github.com>
2022-01-27 02:12:38 +0900

    update

--- src/screens/neuralspace/categorybar.js
+++ src/screens/neuralspace/categorybar.js
@@ -17,7 +17,7 @@ const CategoryWrap = styled(Col)`
 const TaskWrap = styled(Box)`
     width: 100% ;
     height: 130px; 
-    border-radius: 15px;
+    border-radius: 20px;
     position: relative;
     align-items: center;
     justify-content: center;
@@ -32,7 +32,7 @@ export const Task = ({darkMode, content, task}) => {
             <Col justify="center" align="center" position="absolute" zIndex={101} 
                 style={{left: 0, backdropFilter: 'blur(5px)', bottom: 0}}
                 padding="padding: 10px;"
-                width="70%" height="100%" bg={darkMode ? "black" : "white"}>
+                width="70%" height="100%" bg={bgStyler(darkMode)}>
                 <Text className="light" margin="margin: 10px 0px;">{content.title.toUpperCase()}</Text>
                 <Row align="center">
                     <Text size="12.8">20 Right In- Demo</Text>
@@ -45,18 +45,18 @@ export const Task = ({darkMode, content, task}) => {
     )
 }
 
-export const CategoryBar = ({darkMode}) => {
+export const CategoryBar = ({categoryBar, setCategoryBar, darkMode}) => {
     return (
-        <Box position="fixed" bg={bgStyler(darkMode)} padding="padding: 15px;" zIndex={100} width="380px;" 
-            height={Height - 80 + 'px'} style={{right: 0, top: 80, transition: 'all 300ms'}}>
+        <Box position="fixed" padding="padding: 15px;" zIndex={100} width="380px;" 
+            height={Height - 80 + 'px'} style={{right: categoryBar ? 0 : -380, top: 80, transition: 'all 300ms'}}>
             <Flex height="35px;" margin="margin-bottom: 20px;" position="relative">
                 <Flex style={{position: 'absolute', left: 0, bottom: 12}}>
                     <SearchIcon width={16} height={16} stroke="rgb(100, 100, 100)" />
                 </Flex>
                 <Input height="35px;" placeholder="Search" color={darkMode ? "white" : "black"} width="85%" bg="rgba(0,0,0,0)" padding="padding-left: 28px;" style={{border: 0, borderBottomWidth: 1, borderBottomColor: 'rgb(100, 100, 100)', letterSpacing: 0.4, borderBottomStyle: 'solid'}} />
             </Flex>
-            <Flex position="absolute" zIndex={100} style={{right: 15, top: 15}}>
-                <CloseIcon width={30} height={30} bg={bgStyler(darkMode)} stroke="rgb(100, 100, 100)" />
+            <Flex to="cursor" onClick={() => setCategoryBar(!categoryBar)} position="absolute" zIndex={100} style={{right: 15, top: 15}}>
+                <CloseIcon width={30} height={30} bg={darkMode ? "rgb(35, 35, 35)" : "rgb(245, 245, 245)"} stroke={darkMode ? "rgb(100, 100, 100)" : "rgb(120, 120, 120)"} />
             </Flex>
             <Text size="20" className='bold' margin="margin: 10px 0px; margin-bottom: 20px;">TASKS</Text>
             <CategoryWrap scrollbarFalse={true}>
