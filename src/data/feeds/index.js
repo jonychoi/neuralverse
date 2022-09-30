f9679fbc4591f5b4c2eff3fa4aaf1ad181092af7
Jonathan Choi <51044058+koreaunitas@users.noreply.github.com>
2022-09-30 17:21:56 +0900

    update

--- src/data/feeds/index.js
+++ src/data/feeds/index.js
@@ -31,7 +31,7 @@ export default [
         paperswithcode: "https://paperswithcode.com/paper/revisiting-weakly-supervised-pre-training-of",
     },
     {
-        writer_id: users[5],
+        writer_id: users[3],
         title: "Instant Neural Graphics Primitives with a Multiresolution Hash Encoding", 
         description: "Neural graphics primitives, parameterized by fully connected neural networks, can be costly to train and evaluate. We reduce this cost with a versatile new input encoding that permits the use of a smaller network without sacrificing quality, thus significantly reducing the number of floating point and memory access operations: a small neural network is augmented by a multiresolution hash table of trainable feature vectors whose values are optimized through stochastic gradient descent. The multiresolution structure allows the network to disambiguate hash collisions, making for a simple architecture that is trivial to parallelize on modern GPUs. We leverage this parallelism by implementing the whole system using fully-fused CUDA kernels with a focus on minimizing wasted bandwidth and compute operations. We achieve a combined speedup of several orders of magnitude, enabling training of high-quality neural graphics primitives in a matter of seconds, and rendering in tens of milliseconds at a resolution of 1920 X 1080.",
         date: "16 Jan 2022",
