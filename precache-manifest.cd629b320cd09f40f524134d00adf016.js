67998849c111d5a31fa684106bc19f241707f2ef
Jonathan Choi <thesky7@korea.ac.kr>
2022-10-13 14:44:42 +0900

    Updates

---
+++ precache-manifest.cd629b320cd09f40f524134d00adf016.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7fd14bb8ec1e2f43e16",
+    "url": "/neuralverse/static/css/main.bc28c183.chunk.css"
+  },
+  {
+    "revision": "c7fd14bb8ec1e2f43e16",
+    "url": "/neuralverse/static/js/main.c7fd14bb.chunk.js"
+  },
+  {
+    "revision": "fe415ba2c7b72668723e",
+    "url": "/neuralverse/static/js/1.fe415ba2.chunk.js"
+  },
+  {
+    "revision": "ccad2834692b315e78ba",
+    "url": "/neuralverse/static/js/runtime~main.ccad2834.js"
+  },
+  {
+    "revision": "77022b1f3cdf2829799e4d87145511d7",
+    "url": "/neuralverse/static/media/logo.77022b1f.png"
+  },
+  {
+    "revision": "217ad21e925071edb55102daa69b8d87",
+    "url": "/neuralverse/static/media/gan.217ad21e.png"
+  },
+  {
+    "revision": "6177414bb2909e436dc984afcc859bdc",
+    "url": "/neuralverse/static/media/me.6177414b.jpeg"
+  },
+  {
+    "revision": "1ac50d228f7eab81c3279b5770925ffc",
+    "url": "/neuralverse/static/media/city.1ac50d22.jpg"
+  },
+  {
+    "revision": "dc5d8d9007d718f3a3ed1cd960dc29a7",
+    "url": "/neuralverse/static/media/elephants.dc5d8d90.jpg"
+  },
+  {
+    "revision": "36bf930ab5191a8ed62dda9135e4e5c7",
+    "url": "/neuralverse/static/media/zebras.36bf930a.jpg"
+  },
+  {
+    "revision": "ffc5e36161844d3fe50d278cc7e86538",
+    "url": "/neuralverse/static/media/jojo.ffc5e361.png"
+  },
+  {
+    "revision": "e3f97e1369e2ff657e6468ef0e9a4ebc",
+    "url": "/neuralverse/static/media/generate.e3f97e13.svg"
+  },
+  {
+    "revision": "db2e12857e57bfdf73dfcf53b61adfae",
+    "url": "/neuralverse/static/media/logocircle.db2e1285.png"
+  },
+  {
+    "revision": "db06e2f4a6ccb1c12e8c5e451e0bc087",
+    "url": "/neuralverse/static/media/best.db06e2f4.mp4"
+  },
+  {
+    "revision": "d1e7aee69d3761841c93b3c2a78db41a",
+    "url": "/neuralverse/static/media/gd.d1e7aee6.mp4"
+  },
+  {
+    "revision": "ec15cce5e6ac77dabc2ca49ee5fa562a",
+    "url": "/neuralverse/static/media/mountain.ec15cce5.jpeg"
+  },
+  {
+    "revision": "23ab53629b03becbf027304ff2be56e4",
+    "url": "/neuralverse/static/media/monc.23ab5362.jpeg"
+  },
+  {
+    "revision": "b286947f6b09d6d45a8dc1e843f6cf8b",
+    "url": "/neuralverse/static/media/cameramotion.b286947f.png"
+  },
+  {
+    "revision": "9ed7ba636ab8d10afa2990e5934a1d7a",
+    "url": "/neuralverse/static/media/fewshotsegmentation.9ed7ba63.png"
+  },
+  {
+    "revision": "c8491766a2d81aa27c5515ceb4c85855",
+    "url": "/neuralverse/static/media/image classification.c8491766.jpeg"
+  },
+  {
+    "revision": "90f7d919924bb59286d43c06021e938e",
+    "url": "/neuralverse/static/media/objectdetection.90f7d919.jpg"
+  },
+  {
+    "revision": "9b235650676ea0c6e34708d32d6d55c4",
+    "url": "/neuralverse/static/media/yolov3.9b235650.png"
+  },
+  {
+    "revision": "d53478c8070cb9e8cc69a5b733231b62",
+    "url": "/neuralverse/static/media/segmentation.d53478c8.jpg"
+  },
+  {
+    "revision": "4659ed8c64ef7e8d806dad1eed0136c2",
+    "url": "/neuralverse/static/media/style transfer.4659ed8c.jpg"
+  },
+  {
+    "revision": "61921a3c65b2e78b510a752d0860439f",
+    "url": "/neuralverse/static/media/selfsupervb.61921a3c.png"
+  },
+  {
+    "revision": "5d60f59b411dfd300414fee629dbaa9d",
+    "url": "/neuralverse/static/media/super resolution.5d60f59b.jpg"
+  },
+  {
+    "revision": "54021714eba043ed59f6943970901558",
+    "url": "/neuralverse/static/media/generalization.54021714.png"
+  },
+  {
+    "revision": "fcf583ad4142911dba83b9e839ffa418",
+    "url": "/neuralverse/static/media/unsupervised.fcf583ad.png"
+  },
+  {
+    "revision": "05498c509a305b2db4f792968c91aafb",
+    "url": "/neuralverse/static/media/partial.05498c50.png"
+  },
+  {
+    "revision": "b67ca5e8a49781b60f919149dcf57923",
+    "url": "/neuralverse/static/media/background.b67ca5e8.jpeg"
+  },
+  {
+    "revision": "689fa05d88a6e1cb86eba44f87b1ab8b",
+    "url": "/neuralverse/static/media/kaiminghe.689fa05d.jpg"
+  },
+  {
+    "revision": "50d86237151a9bfd07c7e8d481e06c1f",
+    "url": "/neuralverse/static/media/rohit.50d86237.png"
+  },
+  {
+    "revision": "dcae42a0f5945b0c3d75f3ed432f2a64",
+    "url": "/neuralverse/static/media/thomas.dcae42a0.jpg"
+  },
+  {
+    "revision": "76b6db16a5741274aee6ba7e5a4a387d",
+    "url": "/neuralverse/static/media/cvlab.76b6db16.png"
+  },
+  {
+    "revision": "44f88a0d0338c3971d5e18f8dac4ef1a",
+    "url": "/neuralverse/static/media/computer vision.44f88a0d.jpg"
+  },
+  {
+    "revision": "0b49642ee21bd6afb3df4ddaceefd988",
+    "url": "/neuralverse/index.html"
+  }
+];
